7b27cc11b304cf7c4377324a9159a0f6b62ee6b8
RanushMithila <ranushmithila45@gmail.com>
2022-07-07 13:29:42 +0530

    checkout

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+var qty = 0;
+
 function plus(){
-    var qty = parseInt(document.getElementById('qty').value);
+    qty = parseInt(document.getElementById('qty').value);
 
     qty = qty + 1;
 
     document.getElementById('qty').value = qty;
+
+    changePrice();
 }
 
 function minus(){
-    var qty = parseInt(document.getElementById('qty').value);
+    qty = parseInt(document.getElementById('qty').value);
 
     qty = qty - 1;
 
@@ -17,4 +21,20 @@ function minus(){
     }
 
     document.getElementById('qty').value = qty;
+
+    changePrice();
+}
+
+function changePrice(){
+    var price = document.getElementById('pricePerUnit').textContent;
+
+    var price = price.split(" ");
+
+    price = parseFloat(price[1]);
+
+    //alert(price + " " + qty);
+
+    var newPrice = (price * qty).toFixed(2);
+
+    document.getElementById('price').textContent = '$ ' + newPrice;
 }
