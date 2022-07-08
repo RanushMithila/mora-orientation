5c5502aaeabb8355e51b18e2d2e2c4e15b5a8ee5
RanushMithila <ranushmithila45@gmail.com>
2022-07-08 13:25:33 +0530

    cart complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var qty = 0;
+var tot = 0.0;
+var discount = 0.0;
 
 function plus(element){
     qtyID = 'qty['+element+']';
@@ -28,8 +30,8 @@ function minus(element){
 }
 
 function changePrice(element){
-    pricePerID = 'pricePerUnit['+element+']';
-    priceID = 'price['+element+']';
+    var pricePerID = 'pricePerUnit['+element+']';
+    var priceID = 'price['+element+']';
     var price = document.getElementById(pricePerID).textContent;
 
     var price = price.split(" ");
@@ -39,5 +41,50 @@ function changePrice(element){
     var newPrice = (price * qty).toFixed(2);
 
     document.getElementById(priceID).textContent = '$ ' + newPrice;
+    subTot();
+}
+
+function couponValid(){
+    calTot();
+    discount = 0.0;
+    coupon = document.getElementById('couponCode').value;
+    if (coupon == 'ABCDE'){
+        alert('You recieved 10% discount');
+        discount = (tot * 0.1).toFixed(2);
+        document.getElementById('discount').innerHTML = "Discount : " + discount;
+        subTot();
+        //alert(tot);
+    }else{
+        alert('Please enter valid coupon code');
+    }
+}
+
+function calTot(){
+
+    tot = (tot - discount).toFixed(2);
+
+
+    //alert('tot:'+tot);
+    //alert('discount:'+discount);
+
+    document.getElementById('calTot').innerHTML = 'Total: ' + tot;
+
+}
+
+function subTot(){
+    var priceID = '';
+    tot = 0.0;
+    table = document.getElementsByTagName("table")[0];
+    allrows = table.getElementsByTagName("tr").length;
+    itemCnt = allrows - 2;
+    for (i=0; i <= itemCnt; i++) {
+        priceID = 'price['+i+']';
+        var price = document.getElementById(priceID).textContent;
+        price = price.split(" ");
+        tot += parseFloat(price[1]);
+    }
+    tot = tot.toFixed(2);
+    document.getElementById('subTot').innerHTML = 'Total: ' + tot;
+    calTot();
 }
 
